654bd1221471b2af7c6b6c700979f2aee8cb8aed
AF0ns3ca <afonsecahdez@gmail.com>
2023-12-04 09:02:54 +0100

    clase automovilDeportivo

--- js/automovilDeportivo.js
+++ js/automovilDeportivo.js
@@ -1,3 +1,23 @@
-class AutomovilDeportivo extends Vehiculo{
-    
+export class AutomovilDeportivo extends Vehiculo{
+
+    #potenciaMotor;
+
+    constructor(marca, modelo, color, fechaFabricacion, cilindrada, potenciaMotor) {
+        super(marca, modelo, color, fechaFabricacion, cilindrada);
+        this.#potenciaMotor = potenciaMotor;
+      }
+
+      get potenciaMotor() {
+        return this.#potenciaMotor;
+      }
+      set potenciaMotor(potenciaMotor) {
+        this.#potenciaMotor = potenciaMotor;
+      }
+
+      activarModoDeportivo(){
+        return `El ${marca} ${modelo} ha acelerado ha activado el modo deportivo`;
+      }
+    mostrarResultado(){
+        let resultado = super.mostrarResultado()+= `Potencia Motor: ${this.potenciaMotor}`;
+    }
 }
--- js/vehiculo.js
+++ js/vehiculo.js
@@ -1,4 +1,4 @@
-class Vehiculo {
+export class Vehiculo {
   #marca;
   #modelo;
   #color;
@@ -44,8 +44,20 @@ class Vehiculo {
     this.#cilindrada = cilindrada;
   }
 
+  arrancar(){
+    return `El ${marca} ${modelo} ha arrancado`;
+  }
+
+  acelerar(velocidad){
+    return `El ${marca} ${modelo} ha acelerado hasta los ${velocidad} km/h`;
+  }
+
+  frenar(){
+    return `El ${marca} ${modelo} ha acelerado ha frenado`;
+  }
+
   mostrarResultado() {
-    let resultado = `Marca: ${marca} <br> Modelo: ${modelo} <br> Color: ${color} <br> Fecha de Fabricacion: ${fechaFabricacion} <br> Cilindrada: ${cilindrada}`
+    let resultado = `Marca: ${marca} <br> Modelo: ${modelo} <br> Color: ${color} <br> Fecha de Fabricacion: ${fechaFabricacion} <br> Cilindrada: ${cilindrada} <br>`
   }
 
 }
